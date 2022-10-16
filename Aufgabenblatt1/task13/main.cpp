8bda3858e3ed347eb4723011464b2071a0b4d350
SomeStein <pummaaron@gmail.com>
2022-10-17 01:19:04 +0200

    modified:   Aufgabenblatt1/task13/main.cpp 	modified:   Aufgabenblatt1/task13/main.exe

--- Aufgabenblatt1/task13/main.cpp
+++ Aufgabenblatt1/task13/main.cpp
@@ -9,8 +9,7 @@ int main()
    cin >> b;
    cin >> c;
    string res;
-   res =
-       c[c.length() - 1] + b[b.length() - 1] + a[a.length() - 1];
+   res = c[c.length() - 1] + b[b.length() - 1] + a[a.length() - 1];
    // + a.substr(0, a.length() - 1)
    // + b.substr(0, b.length() - 1)
    // + c.substr(0, c.length() - 1);
