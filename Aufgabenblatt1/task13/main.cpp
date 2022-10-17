7beca9a4ac336002484e82dad39ce659deb75d1d
SomeStein <pummaaron@gmail.com>
2022-10-17 22:38:03 +0200

    new file:   .vscode/settings.json 	modified:   Aufgabenblatt1/task13/main.cpp 	modified:   Aufgabenblatt1/task13/main.exe 	modified:   Aufgabenblatt1/task14/main.cpp 	new file:   Aufgabenblatt1/task14/main.exe

--- Aufgabenblatt1/task13/main.cpp
+++ Aufgabenblatt1/task13/main.cpp
@@ -4,12 +4,12 @@ using namespace std;
 int main()
 {
    cout << "Bitte geben sie drei Zahlen ein: ";
-   string a, b, c;
+   int a, b, c;
    cin >> a;
    cin >> b;
    cin >> c;
-   string res;
-   res = c[c.length() - 1] + b[b.length() - 1] + a[a.length() - 1];
+   string res = to_string(c%10) + to_string(b%10) + to_string(a%10);
+   res = res + to_string(a/10) + to_string(b/10) + to_string(c/10);
    // + a.substr(0, a.length() - 1)
    // + b.substr(0, b.length() - 1)
    // + c.substr(0, c.length() - 1);
